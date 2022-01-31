3283f43ef2257f08ade7fe97fc5a48780e595724
Nikita Ermoshkin <nikita@hitl1-1.local>
2022-01-31 13:59:18 -0800

    add more return registers

--- llvm/lib/Target/P2/P2RegisterInfo.cpp
+++ llvm/lib/Target/P2/P2RegisterInfo.cpp
@@ -68,10 +68,11 @@ BitVector P2RegisterInfo::getReservedRegs(const MachineFunction &MF) const {
     Reserved.set(P2::QX);
     Reserved.set(P2::QY);
 
-    Reserved.set(P2::R30); // reserve R30 since it's used for returns
-    Reserved.set(P2::R31); // reserve R31 since it's used for returns
+    // don't think we need to reserve these--they are free to use inside the function if necessary
+    // Reserved.set(P2::R30); // reserve R30 since it's used for returns
+    // Reserved.set(P2::R31); // reserve R31 since it's used for returns
 
-    Reserved.set(P2::R30_R31); // reserve the long long return "register"
+    // Reserved.set(P2::R30_R31); // reserve the long long return "register"
 
     // reserve all "cogram" regsisters
     for (int i = P2::C0; i <= P2::C463; i++) {
